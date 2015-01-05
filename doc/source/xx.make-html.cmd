f6bc28f8bf6803a5846511f8ccbdb350b3b65e94
zoidy <test118@hotmail.com>
2015-01-05 16:32:22 -0500

    update docs. Modify script to move built docs to gh-pages

--- doc/source/xx.make-html.cmd
+++ doc/source/xx.make-html.cmd
@@ -1,4 +1,10 @@
 call xx.make clean
 del /f /s /q autosummary\*
 call xx.make html
+echo about to copy to
+FOR /F %%i IN ("..\..\..\puq gh-pages") DO echo %%~fi\gh pages
+pause
+::try to copy the html to gh-pages folder
+xcopy "_build\html" "..\..\..\puq gh-pages" /Y /E /R /Q
+::rd /s /q _build
 pause
