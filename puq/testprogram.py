6b306d0080c5c09fc3a50565b17b0b1995b33e3c
zoidy <test118@hotmail.com>
2015-01-21 17:27:16 -0500

    fix a bug in writing the paramsFile where spaces in the description would truncate the output when it was subsequently read by numpy

--- puq/testprogram.py
+++ puq/testprogram.py
@@ -205,7 +205,7 @@ class TestProgram(object):
         fname=os.path.join(directory,parsedargs[0].paramsFile)
         f=open(fname,'w')
         for p,v,d in args:
-            f.write("{}\t\t\t{:.9e}\t\t\t{}\n".format(p,v,d))
+            f.write("{}\t{:.9e}\t{}\n".format(p,v,d))
         f.close()
         
         return self.exe
